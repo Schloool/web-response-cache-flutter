ee381ad1d7acdda020975fefdd3822707c90bbfa
Max Schlosser <mschlos3@hs-mittweida.de>
2022-08-21 18:24:00 +0200

    Add new utility cache types

---
+++ lib/web_cache_type/util_web_cache_type.dart
@@ -0,0 +1,18 @@
+import 'package:web_response_cache/web_cache_document.dart';
+import 'package:web_response_cache/web_cache_type/abstract_web_cache_type.dart';
+
+/// A cache type only used when no valid response is available.
+class OnlyWhenNeededCacheType extends AbstractWebCacheType {
+
+  @override
+  bool useCachedDocument(WebCacheDocument cacheDocument) => false;
+}
+
+/// A cache type used whenever possible.
+///
+/// Once a successful response has been saved, it will be used until the cache gets manually reset.
+class WheneverPossibleCacheType extends AbstractWebCacheType {
+
+  @override
+  bool useCachedDocument(WebCacheDocument cacheDocument) => false;
+}
