f0690da8d016504230959b375abb72f269f30bc1
Max Schlosser <mschlos3@hs-mittweida.de>
2022-08-22 12:06:20 +0200

    Adjust header deserialization

--- lib/web_cache_document.dart
+++ lib/web_cache_document.dart
@@ -16,7 +16,7 @@ class WebCacheDocument {
   factory WebCacheDocument.fromJson(Map<String, dynamic> json) {
     return WebCacheDocument(
       creationDate: DateTime.fromMillisecondsSinceEpoch(json['creationDate']),
-      headers: json['headers'],
+      headers: (json['headers'] as Map).cast<String, String>(),
       responseValue: json['responseValue'],
     );
   }
